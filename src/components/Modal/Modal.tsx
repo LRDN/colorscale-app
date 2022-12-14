91323bf2613ba14c84969fe7ef4c04c75cbe442f
LRDN <git@lrdn.net>
2022-12-14 17:44:22 +0100

    Add modal component

---
+++ src/components/Modal/Modal.tsx
@@ -0,0 +1,54 @@
+import clsx from 'clsx'
+import { X } from 'react-feather'
+import { useEffect } from 'react'
+import type { FC, HTMLProps, MouseEvent } from 'react'
+import styles from './Modal.module.scss'
+
+type ComponentProps = HTMLProps<HTMLDivElement> & {
+  onClose: (event: MouseEvent | KeyboardEvent) => void
+  title: string
+}
+
+const Modal: FC<ComponentProps> = ({
+  title,
+  onClose,
+  children,
+  className,
+  ...props
+}) => {
+  const modalClassName = clsx(className, styles.modal)
+  const handleClick = (event: MouseEvent) => onClose(event)
+  const handleBackgroundClick = (event: MouseEvent) => {
+    if (event.target === event.currentTarget) {
+      onClose(event)
+    }
+  }
+
+  useEffect(() => {
+    const handleKeyDown = (event: KeyboardEvent) => {
+      if (event.key === 'Escape') {
+        event.preventDefault()
+        onClose(event)
+      }
+    }
+
+    window.addEventListener('keydown', handleKeyDown)
+    return () => window.removeEventListener('keydown', handleKeyDown)
+  }, [onClose])
+
+  return (
+    <div className={modalClassName} onClick={handleBackgroundClick} {...props}>
+      <div className={styles.modal__content}>
+        <header className={styles.modal__header}>
+          {title}
+          <div className={styles.modal__closeButton} onClick={handleClick}>
+            <X />
+          </div>
+        </header>
+        <div className={styles.modal__body}>{children}</div>
+      </div>
+    </div>
+  )
+}
+
+export default Modal
