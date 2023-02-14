a654c6e796a06d8cba357581f33c103510dbf11c
LRDN <git@lrdn.net>
2023-02-14 16:17:40 +0100

    Add display notice component

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -3,6 +3,7 @@ import Footer from '@components/Footer'
 import type { FC, ReactNode } from 'react'
 import ColorMenu from '@components/ColorMenu'
 import ColorControl from '@components/ColorControl'
+import DisplayNotice from '@components/DisplayNotice'
 import ColorStepControl from '@components/ColorStepControl'
 import styles from './Layout.module.scss'
 
@@ -12,20 +13,23 @@ type ComponentProps = {
 
 const Layout: FC<ComponentProps> = ({ children }) => {
   return (
-    <div className={styles.layout}>
-      <aside className={styles.layout__sidebar}>
-        <Header />
-        <ColorMenu />
-        <Footer />
-      </aside>
-      <main className={styles.layout__body}>{children}</main>
-      <aside className={styles.layout__sidebar}>
-        <ColorStepControl />
-        <ColorControl channel="hue" />
-        <ColorControl channel="saturation" />
-        <ColorControl channel="brightness" />
-      </aside>
-    </div>
+    <>
+      <div className={styles.layout}>
+        <aside className={styles.layout__sidebar}>
+          <Header />
+          <ColorMenu />
+          <Footer />
+        </aside>
+        <main className={styles.layout__body}>{children}</main>
+        <aside className={styles.layout__sidebar}>
+          <ColorStepControl />
+          <ColorControl channel="hue" />
+          <ColorControl channel="saturation" />
+          <ColorControl channel="brightness" />
+        </aside>
+      </div>
+      <DisplayNotice />
+    </>
   )
 }
 
